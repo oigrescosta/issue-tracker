cf439e0081ab133a0f50c4d727d4b366e2c1f01c
Sérgio Costa <costa.sergio.86@gmail.com>
2023-12-22 19:11:21 +0000

    Add the login and logout links

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -5,11 +5,13 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
 import classNames from 'classnames';
+import { useSession } from 'next-auth/react';
+import { Box } from '@radix-ui/themes';
 
 const Navbar = () => {
   // This gets current path from Navbar
   const currentPath = usePathname();
-  console.log(currentPath);
+  const {status, data: session} = useSession();
 
   const links = [
     { label: 'Dashboard', href: '/' },
@@ -23,17 +25,21 @@ const Navbar = () => {
             {links.map(link =>
                 // Fetch the links with an iteration using the map
                 // Pass the link as an argument to fetch the key that contains the value
-                <Link
-                    key={link.href}
-                    className={classNames({
-                      'text-zinc-900': link.href === currentPath,
-                      'text-zinc-500': link.href !== currentPath,
-                      'hover:text-zinc-800 transition-colors': true
-                    })}
-                    href={link.href}>{link.label}</Link>
+                <li key={link.href}>
+                  <Link
+                      className={classNames({
+                        'text-zinc-900': link.href === currentPath,
+                        'text-zinc-500': link.href !== currentPath,
+                        'hover:text-zinc-800 transition-colors': true
+                      })}
+                      href={link.href}>{link.label}</Link>
+                  </li>
             )}
-
         </ul>
+        <Box>
+          { status === "authenticated" && <Link href="/api/auth/signout">Log out</Link> }
+          { status === "unauthenticated" && <Link href="/api/auth/signin">Login</Link> }
+        </Box>
     </nav>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Container, Theme, ThemePanel } from '@radix-ui/themes';
 import Navbar from './Navbar'
+import AuthProvider from './auth/Provider';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -24,14 +25,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <Theme appearance="light" accentColor="violet">
-         <Navbar />
-          <main className='p-5'>
-            <Container>
-               {children}
-            </Container>
-          </main>
-        </Theme>
+        <AuthProvider>
+          <Theme appearance="light" accentColor="violet">
+            <Navbar />
+              <main className='p-5'>
+                <Container>
+                  {children}
+                </Container>
+              </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   )
