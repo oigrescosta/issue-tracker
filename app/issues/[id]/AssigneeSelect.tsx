196cff7baba6677f6ef14ceabf996c8bf2349b21
Sérgio Costa <costa.sergio.86@gmail.com>
2023-12-28 11:05:10 +0000

    Refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,37 +1,33 @@
 'use client';
-import { User, Issue } from '.prisma/client';
-import { Select } from '@radix-ui/themes'
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { Issue, User } from '.prisma/client';
 import { Skeleton } from '@/app/components';
+import { Select } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import toast, { Toaster } from 'react-hot-toast';
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: () => axios.get('/api/users').then(res => res.data),
-    staleTime: 60 * 1000, //60s
-    retry: 3
-  });
+  const { data: users, error, isLoading } = useUsers();
 
   if(isLoading) return <Skeleton />;
 
   if (error) return null;
 
+  const assignIssue = async (userId: String) => {
+    try {
+      await axios.patch('/api/issues/' + issue.id, {
+        assignedToUserId: userId || null,
+      })
+    } catch (error) {
+      toast.error('Changes could not be saved.');
+    }
+}
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || ""}
-        onValueChange={async (userId) => {
-          try {
-            await axios.patch('/api/issues/' + issue.id, {
-              assignedToUserId: userId || null,
-            })
-          } catch (error) {
-            toast.error('Changes could not be saved.');
-          }
-      }}>
+        onValueChange={assignIssue}>
           <Select.Trigger placeholder='Assign' />
           <Select.Content>
               <Select.Group>
@@ -47,5 +43,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   )
 }
+
+const useUsers = () =>
+  useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: () => axios.get('/api/users').then(res => res.data),
+  staleTime: 60 * 1000, //60s
+  retry: 3
+})
 
 export default AssigneeSelect
