c6df90fb16dfa871d2eb6946895e3311fbda6512
Sérgio Costa <costa.sergio.86@gmail.com>
2023-12-29 21:21:33 +0000

    Build the LatestIssue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-interface Props {
-  searchParams: {
-    page: number;
-  }
-}
-
-export default function Home({ searchParams }: Props) {
+export default function Home() {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={searchParams.page}/>
+    <LatestIssues />
   )
 }
