458a15297070322d34de05304ec885a4d48c264b
Sérgio Costa <costa.sergio.86@gmail.com>
2023-12-29 18:13:32 +0000

    Refactor: Extract IssueTable

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,12 @@ import Pagination from "./components/Pagination";
 
 interface Props {
   searchParams: {
-    page: String;
+    page: number;
   }
 }
 
 export default function Home({ searchParams }: Props) {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)}/>
+    <Pagination itemCount={100} pageSize={10} currentPage={searchParams.page}/>
   )
 }
